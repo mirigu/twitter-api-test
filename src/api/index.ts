7d0e2594eaf59be2a39b76bc3b6efb7f4db4c013
MiriGu <miriggu9@gmail.com>
2022-10-28 22:37:05 +0900

    ✨ [add] env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const instance = axios.create({
-  // baseURL: "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 instance.defaults.headers.post["Content-Type"] = "application/json";
