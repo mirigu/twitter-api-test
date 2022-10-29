0f21977e2f6f3bfaf1e7c0583902c9d7942e8916
MiriGu <miriggu9@gmail.com>
2022-10-29 22:42:26 +0900

    ✨ 💄 [add] landing

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,7 +1,40 @@
+import Head from "next/head";
 import React from "react";
+import styles from "../../../styles/Home.module.css";
+import { authClient, STATE } from "../../pages";
 
 const Landing = () => {
-  return <React.Fragment></React.Fragment>;
+  const authUrl = authClient.generateAuthURL({
+    state: STATE,
+    code_challenge_method: "s256",
+  });
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title></title>
+        <meta name="Twitter API Test" content="Twitter API Test" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <a>Twitter</a> API Test
+        </h1>
+        <p className={styles.description}>
+          Twitter API Test
+          <code className={styles.code}>
+            <a>Twitter API</a>
+          </code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href={authUrl} className={styles.card}>
+            <h2>Login</h2>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Landing;
