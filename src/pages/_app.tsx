b3e894a2b57685b7affe68034176345b3556e183
MiriGu <miriggu9@gmail.com>
2022-10-30 19:39:07 +0900

    ✨ [add] redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import "../../styles/globals.css";
+import { Provider } from "react-redux";
+// redux
+import { store } from "../redux/store";
+// types
 import type { AppProps } from "next/app";
+// css
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ src/redux/store.ts
@@ -0,0 +1,24 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+export interface IReduxState {}
+
+export type AsyncThunkConfig = {
+  state: IReduxState;
+  rejectValue?: string;
+};
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: true,
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
